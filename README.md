2036a2e5747084c3a53a12680d4be6d20715e7ce
Andrew McCloud <andrew@amccloud.com>
2018-10-21 18:30:45 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# A GraphQL server example
+
+>  **A simple, straight forward guide to building a GraphQL server with Apollo Server.**
+
+## About this repository
+
+This repository is the example application for the [_Getting Started_ guide](https://www.apollographql.com/docs/apollo-server/v2/getting-started.html) in the Apollo Server documentation.
+
+## Online sandbox
+
+Play with this example using [Glitch](https://glitch.com/) by clicking the following button:
+
+[![Remix on Glitch](https://cdn.glitch.com/2703baf2-b643-4da7-ab91-7ee2a2d00b5b%2Fremix-button.svg)](https://glitch.com/edit/#!/import/github/apollographql/graphql-server-example)
+
+## Installation
+
+1. Clone this repository: `git clone git@github.com:apollographql/graphql-server-example.git`
+2. `cd` into the `graphql-server-example` folder and run the following commands:
+    1. Run `npm install`
+    2. Run `node index.js`
+3. Open your browser and see the server running on `localhost:4000/graphql` with the GraphQL playground.
+
+## Documentation
+
+Check out the [official Apollo Server documentation](https://www.apollographql.com/docs/apollo-server/v2/) for more information.
---
+++ index.js
@@ -0,0 +1,83 @@
+const fs = require("fs");
+const path = require("path");
+const { ApolloServer } = require("apollo-server");
+const Abstract = require("abstract-sdk");
+
+function resolversForAbstractClient({ only, ignore } = {}) {
+  const transport = new Abstract.TRANSPORTS.API();
+
+  return Reflect.ownKeys(transport).reduce(
+    (schema, endpointKey) => {
+      const transportEndpoint = transport[endpointKey];
+
+      // Ignore non-endpoints
+      if (
+        !transportEndpoint ||
+        ["string", "number", "boolean"].includes(typeof transportEndpoint)
+      ) {
+        return schema;
+      }
+
+      Reflect.ownKeys(transportEndpoint).forEach(methodKey => {
+        const property = `${endpointKey}.${methodKey}`;
+
+        if (only && !only.includes(property)) return;
+        if (ignore && ignore.includes(property)) return;
+
+        const endpointResolverKey = `${endpointKey.charAt(0).toUpperCase() +
+          endpointKey.slice(1)}Endpoint`;
+
+        schema[endpointResolverKey] = schema[endpointResolverKey] || {};
+
+        if (["create", "update", "delete"].includes(methodKey)) {
+          schema.Mutation[endpointKey] = (root, args, context) =>
+            context.abstract[endpointKey];
+
+          schema[endpointResolverKey][methodKey] = (endpoint, args) =>
+            Promise.resolve(
+              endpoint[methodKey](
+                args.objectDescriptor,
+                args.input,
+                args.options
+              )
+            );
+        } else {
+          schema.Query[endpointKey] = (root, args, context) =>
+            context.abstract[endpointKey];
+
+          schema[endpointResolverKey][methodKey] = (endpoint, args) =>
+            Promise.resolve(
+              endpoint[methodKey](args.objectDescriptor, args.options)
+            );
+        }
+      });
+
+      return schema;
+    },
+    { Query: {}, Mutation: {} }
+  );
+}
+
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(path.resolve(__dirname, "schema.graphql"), "utf8"),
+  resolvers: resolversForAbstractClient(),
+  context: ({ req }) => {
+    const [_, abstractToken] = (req.headers.authorization || "").split(" ", 2);
+
+    return {
+      abstract: abstractToken
+        ? Abstract.Client({
+            transport: Abstract.TRANSPORTS.API, // TODO: remove
+            abstractToken
+          })
+        : undefined
+    };
+  },
+  engine: process.env.ENGINE_API_KEY && {
+    apiKey: process.env.ENGINE_API_KEY
+  }
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
