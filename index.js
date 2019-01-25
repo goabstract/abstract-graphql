1361f4145f770b10c815a20393507ea8e27038f3
Andrew McCloud <andrew@amccloud.com>
2019-01-24 20:39:36 -0800

    Update sdk to 0.6.0

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const { ApolloServer } = require("apollo-server");
 const Abstract = require("abstract-sdk");
 
 function resolversForAbstractClient({ only, ignore } = {}) {
-  const transport = new Abstract.TRANSPORTS.API();
+  const transport = new Abstract.TRANSPORTS.API({
+    accessToken: "NOT_AUTHED"
+  });
 
   return Reflect.ownKeys(transport).reduce(
     (schema, endpointKey) => {
@@ -21,27 +23,35 @@ function resolversForAbstractClient({ only, ignore } = {}) {
       Reflect.ownKeys(transportEndpoint).forEach(methodKey => {
         const property = `${endpointKey}.${methodKey}`;
 
+        if (endpointKey === "accessToken") return;
         if (only && !only.includes(property)) return;
         if (ignore && ignore.includes(property)) return;
 
-        const endpointResolverKey = `${endpointKey.charAt(0).toUpperCase() +
-          endpointKey.slice(1)}Endpoint`;
-
-        schema[endpointResolverKey] = schema[endpointResolverKey] || {};
-
         if (["create", "update", "delete"].includes(methodKey)) {
+          const endpointResolverKey = `${endpointKey.charAt(0).toUpperCase() +
+            endpointKey.slice(1)}MutationEndpoint`;
+
+          schema[endpointResolverKey] = schema[endpointResolverKey] || {};
+
           schema.Mutation[endpointKey] = (root, args, context) =>
             context.abstract[endpointKey];
 
-          schema[endpointResolverKey][methodKey] = (endpoint, args) =>
-            Promise.resolve(
+          schema[endpointResolverKey][methodKey] = (endpoint, args) => {
+            console.log(endpoint, args)
+            return Promise.resolve(
               endpoint[methodKey](
                 args.objectDescriptor,
-                args.input,
-                args.options
+                args.input ? args.input : args.options,
+                args.input ? args.options : undefined,
               )
             );
+          }
         } else {
+          const endpointResolverKey = `${endpointKey.charAt(0).toUpperCase() +
+            endpointKey.slice(1)}Endpoint`;
+
+          schema[endpointResolverKey] = schema[endpointResolverKey] || {};
+
           schema.Query[endpointKey] = (root, args, context) =>
             context.abstract[endpointKey];
 
@@ -62,11 +72,13 @@ const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.resolve(__dirname, "schema.graphql"), "utf8"),
   resolvers: resolversForAbstractClient(),
   context: ({ req }) => {
-    const [_, accessToken] = (req.headers.authorization || "").split(" ", 2);
+    const [_, accessToken = process.env.ABSTRACT_TOKEN] = (
+      req.headers.authorization || ""
+    ).split(" ", 2);
 
     return {
       abstract: accessToken
-        ? Abstract.Client({
+        ? Abstract.client({
             transport: Abstract.TRANSPORTS.API, // TODO: remove
             accessToken
           })
